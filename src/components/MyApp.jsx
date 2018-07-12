d4a4863dbc0130203b033749044eb8545f8f7ef2
pashebor <pashebor@salesgenerator.pro>
2018-07-12 21:18:21 +0300

    test

--- src/components/MyApp.jsx
+++ src/components/MyApp.jsx
@@ -4,9 +4,8 @@ import DefaultButton from './elements/buttons/DefaultButton';
 class MyApp extends React.Component{
     render() {
         return(
-            <main>
-                <h1 className={'title icon icon-heart'}>Hello Juli!</h1>
-                <DefaultButton message={'I\'m here now !'} />
+            <main className={'container'}>
+            
             </main>
         )
     }
